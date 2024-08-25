2b9443732741f581497652f522bbb54c92d5a40b
lucasact4 <funcraftact4@outlook.com>
2024-08-25 03:44:21 -0300

    added service scrolling

--- script.js
+++ script.js
@@ -30,3 +30,48 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+document.getElementById('service-1').addEventListener('click', function() {
+  showServiceContent('content-1');
+});
+document.getElementById('service-2').addEventListener('click', function() {
+  showServiceContent('content-2');
+});
+document.getElementById('service-3').addEventListener('click', function() {
+  showServiceContent('content-3');
+});
+document.getElementById('service-4').addEventListener('click', function() {
+  showServiceContent('content-4');
+});
+document.getElementById('service-5').addEventListener('click', function() {
+  showServiceContent('content-5');
+});
+document.getElementById('service-6').addEventListener('click', function() {
+  showServiceContent('content-6');
+});
+document.getElementById('service-7').addEventListener('click', function() {
+  showServiceContent('content-7');
+});
+document.getElementById('service-8').addEventListener('click', function() {
+  showServiceContent('content-8');
+});
+document.getElementById('service-9').addEventListener('click', function() {
+  showServiceContent('content-9');
+});
+document.getElementById('service-10').addEventListener('click', function() {
+  showServiceContent('content-10');
+});
+
+function showServiceContent(contentId) {
+  var contents = document.querySelectorAll('.service-content');
+  contents.forEach(function(content) {
+      content.classList.remove('active');
+  });
+
+  document.getElementById(contentId).classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  showServiceContent('content-10');
+});
